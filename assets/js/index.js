a95f979b38fd6266823972d8d7b6ac77eb1d72f6
magp96 <marlon.Agualteros@gmail.com>
2023-05-19 21:07:41 -0500

    Agregando funcionalidad al sitio web mediante JavaScript

---
+++ assets/js/index.js
@@ -0,0 +1,39 @@
+const $formulario = document.getElementById('formulario');
+const $name = document.getElementById('name');
+const $email = document.getElementById('email');
+const $tel = document.getElementById('tel');
+const $btnsend = document.getElementById('btnsend');
+const $confirmar = document.getElementById('confirmar');
+
+let user = {
+    nameUser : null,
+    email : null,
+    tel :  null
+}
+$name.addEventListener('change', (e) => {
+    user.nameUser = e.target.value;
+    console.log(user.nameUser);
+});
+
+$email.addEventListener('change', (e) => {
+    user.email = e.target.value;
+    console.log(user.email);
+});
+
+$tel.addEventListener('change', (e) => {
+    user.tel = e.target.value;
+    console.log(user.tel);
+});
+
+$btnsend.addEventListener('click', (e) => {
+    if (user.nameUser,user.email,user.tel) {
+        $confirmar.innerText = '✅ Resgistro exitoso en breve nos comunicaremos con tigo';
+        console.log(user);
+
+        $formulario.reset();
+    } else {
+        $confirmar.innerText = '❌ complete todos los campos'
+    }
+    e.preventDefault();
+
+});
